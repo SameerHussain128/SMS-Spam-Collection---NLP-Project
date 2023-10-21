956cc3083f6f9e0c930a01595802044c044bcd4b
Mohd Sameer Hussain Hashmi <133598055+SameerHussain128@users.noreply.github.com>
2023-10-21 15:55:34 +0530

    Add files via upload

---
+++ spamcollection_stemming.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+
+messages = pd.read_csv(r'C:\Users\SAMEER\Projects\NLP\SMS Spam collection\spam.csv',encoding='latin-1')
+
+messages
+
+# droping unnessary unnamed columns
+messages = messages.drop('Unnamed: 2', axis=1)
+messages = messages.drop('Unnamed: 3', axis=1)
+messages = messages.drop('Unnamed: 4', axis=1)
+
+#Data cleaning and preprocessing
+import re
+import nltk
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+ps = PorterStemmer()
+wordnet=WordNetLemmatizer()
+corpus = []
+for i in range(0, len(messages)):
+    review = re.sub('[^a-zA-Z]', ' ', messages['message'][i])
+    review = review.lower()
+    review = review.split()
+    
+    review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=5000)
+X = cv.fit_transform(corpus).toarray()
+
+y=pd.get_dummies(messages['label'])
+y=y.iloc[:,1].values
+
+# Train Test Split
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Training model using Naive bayes classifier
+
+from sklearn.naive_bayes import MultinomialNB
+spam_detect_model = MultinomialNB().fit(X_train, y_train)
+
+y_pred=spam_detect_model.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+ac=accuracy_score(y_test, y_pred)
